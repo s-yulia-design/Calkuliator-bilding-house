32a8197d62b819328052adf738dd86785825b46b
s-yulia-design <s-yulia-design@users.noreply.github.com>
2026-07-18 17:55:09 +0000

    deploy: 8b70c8acb8aaa2842cb97f7447b6ba59776a83fb

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Calkuliator-bilding-house/sw.js', { scope: '/Calkuliator-bilding-house/' })})}
